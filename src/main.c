29f495d9ad940bc4daee980b3416acd8d858c422
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-02-17 19:21:36 +0545

    Fixed segmentation error, using printTable function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include "schema.h"
 #include "input.h"
+#include "output.h"
 
 int main(){
   Data d1, d2, d3;
@@ -20,15 +21,9 @@ int main(){
     {5, "point", FLOAT},
     {4, "Text", STRING}
   };
-  Table table = {3,3, col, row};
+  Table table = {3,1, col, row};
   // welcomeMessage();
-  printf("%d\n", table.row[0].field[0].data.i);
-  printf("%f\n", table.row[0].field[1].data.f);
-  printf("%s\n", table.row[0].field[2].data.c);
-  printf("\n");
-  printf("%s\n", table.column[0].name);
-  printf("%s\n", table.column[1].name);
-  printf("%s\n", table.column[2].name);
+  printTable(table);
 
   return 0;
 }
