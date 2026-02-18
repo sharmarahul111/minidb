eea4aba507256fd55293694af6fbf2f87f100848
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-02-18 12:58:41 +0545

    using loop to generate data

--- src/dummy_table.c
+++ src/dummy_table.c
@@ -4,32 +4,34 @@
 Table* dummy_table(){
   int table_count = 1;
   int column_count = 3;
-  int row_count = 1;
+  int row_count = 4;
   int field_count = 3;
   Table *tp;
   Column *cp;
   Row *rp;
   Field *fp;
-  tp = (Table *) malloc(table_count*sizeof(Table));
-  cp = (Column *) malloc(column_count*sizeof(Column));
-  rp = (Row *) malloc(row_count*sizeof(Row));
-  fp = (Field *) malloc(field_count*sizeof(Field));
-
-  Data d1, d2, d3;
-  d1.i = 24;
-  d2.f = 55.66;
-  d3.c = "Hello";
-
-  fp[0] = (Field) {1, INT, d1};
-  fp[1] = (Field) {1, FLOAT, d2};
-  fp[2] = (Field) {5, STRING, d3};
+  tp = (Table *) malloc(table_count * sizeof(Table));
+  cp = (Column *) malloc(column_count * sizeof(Column));
+  rp = (Row *) malloc(row_count * sizeof(Row));
+  fp = (Field *) malloc(field_count * row_count * sizeof(Field));
 
   cp[0] = (Column) {2, "ID", INT};
-  cp[1] = (Column) {5, "point", FLOAT};
+  cp[1] = (Column) {5, "Float", FLOAT};
   cp[2] = (Column) {4, "Text", STRING};
 
-  rp[0] = (Row) {3, fp};
 
-  tp[0] = (Table) {3,1, cp, rp};
+  char *names[] = {"Mark", "Jake", "David", "Sully"};
+  float points[] = {34.65, 240.63, 473.34, 412.64};
+  int i;
+  for (i=0;i<row_count;i++) {
+    fp[i*field_count+0] = (Field) {1, INT, (Data) {.i = i+1}};
+    fp[i*field_count+1] = (Field) {1, FLOAT, (Data) {.f = points[i]}};
+    fp[i*field_count+2] = (Field) {10, STRING, (Data) {.c = names[i]}};
+
+    rp[i] = (Row) {field_count, fp+i*field_count};
+  }
+
+  tp[0] = (Table) {column_count, row_count, cp, rp};
   return tp;
 }
+
